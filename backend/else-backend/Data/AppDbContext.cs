f6dec669fe77e98e0ad8fab33aa91df00056dc4f
LindaAmnell <amnell3@hotmail.com>
2026-05-07 10:47:36 +0200

    added dbContext

---
+++ backend/else-backend/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using else_backend.Data.Enties;
+using Microsoft.EntityFrameworkCore;
+
+namespace else_backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+
+
+        }
+
+        public DbSet<TodoItem> todoItems => Set<TodoItem>();
+
+
+    }
+}
