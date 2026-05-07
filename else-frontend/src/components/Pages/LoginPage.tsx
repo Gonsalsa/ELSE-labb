bb97295aca3ef5e85766dc40c4f9595392ccb561
Emma Wall <emma.wall98@gmail.com>
2026-05-07 09:21:49 +0200

    Created loginpage

---
+++ else-frontend/src/components/Pages/LoginPage.tsx
@@ -0,0 +1,18 @@
+import "../../css/LoginPage.module.css";
+
+const LoginPage = () => {
+  return (
+    <section>
+      <div className="title">
+        <h1>Login</h1>
+      </div>
+      <form>
+        <label>Username</label>
+        <input />
+        <label>Password</label>
+        <input />
+      </form>
+    </section>
+  );
+};
+export default LoginPage;
