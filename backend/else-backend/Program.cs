8075a70040ad30cfad107df380bebdab59e70bd9
Gonsalsa Laptop <sebgon02@gmail.com>
2026-05-07 13:17:38 +0200

    Connected to the database and made a migration to it

--- backend/else-backend/Program.cs
+++ backend/else-backend/Program.cs
@@ -3,14 +3,23 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connString = builder.Configuration
+    .GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("No ConnString");
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+    options.UseSqlServer(connString));
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    db.Database.Migrate();
+}
+
 app.UseHttpsRedirection();
-
-
+app.MapControllers();
 app.Run();
 
