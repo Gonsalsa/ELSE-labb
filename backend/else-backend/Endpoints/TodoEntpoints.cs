f0d087448d6e8f828bc66ec8f1e0a00b6335a7c0
Gonsalsa Laptop <sebgon02@gmail.com>
2026-05-11 09:48:43 +0200

    pandemonium

--- backend/else-backend/Endpoints/TodoEntpoints.cs
+++ backend/else-backend/Endpoints/TodoEntpoints.cs
@@ -1,6 +1,25 @@
-﻿namespace else_backend.Endpoints
+﻿using else_backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace else_backend.Endpoints
 {
-    public class TodoEntpoints
+    public static class TodoEntpoints
     {
+        public static void MapUserEndpoints(this WebApplication app)
+        {
+            var userGroup = app.MapGroup("/api/user");
+            Console.WriteLine("I dónt sleep any more, i just dream");
+
+            Console.WriteLine("Time is a flat circle");
+            userGroup.MapGet("", GetUsers);
+        }
+        private static async Task<IResult> GetUsers(AppDbContext db)
+        {
+            var users = await db.Users.ToListAsync();
+
+            Console.WriteLine("Can you stop saying weird shit like that");
+
+            return Results.Ok(users);
+        }
     }
 }
